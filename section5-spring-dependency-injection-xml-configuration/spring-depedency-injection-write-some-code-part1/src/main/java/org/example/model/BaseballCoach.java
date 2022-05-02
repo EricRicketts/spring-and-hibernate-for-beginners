3170019d867b2f46c6554300909a74e5f0d1969b
Eric Ricketts <eric_ricketts@me.com>
2022-05-02 08:27:03 -0400

    constructor injection part 1 setup classes for dependencies

---
+++ section5-spring-dependency-injection-xml-configuration/spring-depedency-injection-write-some-code-part1/src/main/java/org/example/model/BaseballCoach.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+public class BaseballCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "30 minutes of batting practice.";
+    }
+
+    @Override
+    public String getDailyFortune() {
+
+    }
+}
---
+++ section5-spring-dependency-injection-xml-configuration/spring-depedency-injection-write-some-code-part1/src/main/java/org/example/model/TrackCoach.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+public class TrackCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k.";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        
+    }
+}
