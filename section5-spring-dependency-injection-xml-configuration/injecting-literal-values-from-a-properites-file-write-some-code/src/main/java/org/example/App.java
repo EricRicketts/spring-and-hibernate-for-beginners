ff05fcc6fb932b6bf5c79c328b510e39a5a3575a
Eric Ricketts <eric_ricketts@me.com>
2022-05-02 11:41:58 -0400

    injecting values from a property file

---
+++ section5-spring-dependency-injection-xml-configuration/injecting-literal-values-from-a-properites-file-write-some-code/src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.example.model.BaseballCoach;
+import org.example.model.CricketCoach;
+import org.example.model.TrackCoach;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main( String[] args ) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        BaseballCoach baseballCoach = context.getBean("baseballCoach", BaseballCoach.class);
+        TrackCoach trackCoach = context.getBean("trackCoach", TrackCoach.class);
+        CricketCoach cricketCoach = context.getBean("cricketCoach", CricketCoach.class);
+        String output = baseballCoach.getDailyWorkout() + "\n" + baseballCoach.getDailyFortune() + "\n"
+                + trackCoach.getDailyWorkout() + "\n" + trackCoach.getDailyFortune() + "\n"
+                + cricketCoach.getDailyWorkout() + "\n" + cricketCoach.getDailyFortune() + "\n"
+                + cricketCoach.getEmailAddress() + "\n" + cricketCoach.getTeam();
+        System.out.println(output);
+        context.close();
+    }
+}
---
+++ section5-spring-dependency-injection-xml-configuration/injecting-literal-values-from-a-properites-file-write-some-code/src/main/java/org/example/model/CricketCoach.java
@@ -0,0 +1,41 @@
+package org.example.model;
+
+import org.example.services.FortuneService;
+
+public class CricketCoach implements Coach {
+
+    private FortuneService fortuneService;
+    private String emailAddress;
+    private String team;
+
+    public CricketCoach() { super(); }
+
+    public void setFortuneService(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes.";
+    }
+
+    public String getDailyFortune() {
+        return this.fortuneService.getFortune();
+    }
+}
