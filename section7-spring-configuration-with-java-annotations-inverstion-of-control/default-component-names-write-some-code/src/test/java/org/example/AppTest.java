97c4f090b6ce75623000e5f2b64c2aadd0ba228e
Eric Ricketts <eric_ricketts@me.com>
2022-05-07 09:49:28 -0400

    default component names

---
+++ section7-spring-configuration-with-java-annotations-inverstion-of-control/default-component-names-write-some-code/src/test/java/org/example/AppTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.example.model.PickleBallCoach;
+import org.example.model.TennisCoach;
+import org.example.model.TrackCoach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("/applicationContext.xml")
+public class AppTest {
+
+    String expected;
+    @Test
+    public void testDefaultComponentName() {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        TennisCoach coach = context.getBean("tennisCoach", TennisCoach.class);
+        expected = "Practice backhand volley for 30 minutes.";
+        assertEquals(expected, coach.getDailyWorkout());
+    }
+
+}
