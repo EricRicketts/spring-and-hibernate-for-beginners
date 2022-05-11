4474df1fefbfd4f8976fc97ee59eab65c74a8058
Eric Ricketts <eric_ricketts@me.com>
2022-05-11 04:23:40 -0400

    injecting properties from a file

--- section10-spring-configuration-with-java-code-no-xml/injecting-values-from-properties-file-write-some-code-parts-1-and-2/src/test/java/org/example/AppTest.java
+++ section10-spring-configuration-with-java-code-no-xml/injecting-values-from-properties-file-write-some-code-parts-1-and-2/src/test/java/org/example/AppTest.java
@@ -1,20 +1,34 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.example.config.ProjectConfig;
+import org.example.model.JavaCoach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {ProjectConfig.class})
+public class AppTest {
+
+    ApplicationContext context;
+    JavaCoach javaCoach;
+    String[] expected;
+    String[] results;
+    @BeforeEach
+    public void setUp() {
+        context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        javaCoach = context.getBean("javaCoach", JavaCoach.class);
+    }
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testLoadPropertiesFromFile() {
+        expected = new String[]{"eric_ricketts@icloud.com", "New To Java"};
+        results = new String[]{javaCoach.getEmail(), javaCoach.getTeam()};
+        assertArrayEquals(expected, results);
     }
 }
